84b5b2815694132d3ebc0fec25bdd292821a373a
catwang01 <2015201620@ruc.edu.cn>
2020-10-19 17:39:01 +0800

    add test

---
+++ LocalNote/test/test_case.py
@@ -0,0 +1,21 @@
+import pytest
+import pexpect
+
+class TestCase():
+
+    def test_1_init(self):
+        token = "S=s54:U=157132c:E=16bd2587947:C=16bae4bf530:P=1cd:A=en-devtoken:V=2:H=e9d6084e80270661c8fdd80fca2896b9"
+        root_path = "/Users/ed/Git/LocalNote/LocalNote/test/evernote"
+        main_path = "/Users/ed/Git/LocalNote/LocalNote/main.py"
+        cmd = "cd {} && python {} init".format(root_path, main_path)
+        # child = pexpect.spawn(cmd)
+        # child.expect("是否是沙盒环境？")
+        # child.sendline('y')
+        # child.expect('是否使用开发者Token？')
+        # child.sendline('y')
+        # child.expect('开发者Token:')
+        # child.sendline(token)
+
+
+if __name__ == "__main__":
+    pytest.main(["-s"])
